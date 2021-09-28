c723e2ff40c87f1fa40821810c17817ab353c6ab
Mohammad Atallah <mohamedatallahh2017@gmail.com>
2021-09-28 19:35:57 +0200

    Project setup

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="header__nav">
+      <div className="container">
+        <h1>Navbar</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <section className="products">
+      <div className="container">
+        <h2 className="products__title">Featured products.</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Products;
